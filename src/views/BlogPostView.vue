87c7a28cca03864c87775195ef3c459e70318f4c
장두혁 <jjd0324@gmail.com>
2022-11-26 21:39:26 +0900

    STYLE BlogPostView.vue html/css

--- src/views/BlogPostView.vue
+++ src/views/BlogPostView.vue
@@ -27,6 +27,38 @@
       <div class="p-2 flex-fill btn left-btn">이전포스트</div>
       <div class="p-2 flex-fill btn right-btn">다음포스트</div>
     </div>
+    <div class="comment-div">
+      <h4 class="comment-head">2개의 댓글</h4>
+      <div class="comment-body">
+        <div class="comment-input">
+          <textarea class="comment-text" placeholder="댓글을 입력하세요"></textarea>
+          <div class="comment-button">
+            <button class="btn btn-outline-dark">댓글 작성</button>
+          </div>
+        </div>
+        <div>
+          <div class="comment-list">
+            <div v-for="item in 3" :key="item" class="commnet">
+              <div class="commnet-top">
+                <div class="commnet-info">
+                  <span class="post-name">jang_du</span>
+                  <span>2022.12.12</span>
+                </div>
+                <div class="head-btns">
+                  <button class="head-btn">수정</button>
+                  <button class="head-btn">삭제</button>
+                </div>
+              </div>
+              <div class="commnet-content">
+                <div>
+                  <p>aaaaaanvkndvkalsnn;a</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -143,5 +175,58 @@ export default {
   margin: 0 auto;
   margin-top: 1rem;
   margin-bottom: 1rem;
+}
+
+.comment-div{
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 5rem;
+}
+.comment-input{
+  margin-bottom: 5rem;
+}
+.comment-button{
+  display: flex;
+  -webkit-box-pack: end;
+  justify-content: flex-end;
+}
+.comment-text{
+  resize: none;
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  min-height: 7rem;
+  outline: none;
+  border: 1px solid rgb(138, 138, 138, .5);
+  margin-bottom: 2rem;
+  font-size: 1rem;
+  width: 100%;
+  line-height: 2;
+  background: rgb(255, 247, 232);
+}
+.commnet{
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+  border-top: 1px solid rgb(165, 165, 165, .6);
+}
+.commnet-top{
+  margin-bottom: 1.8rem;
+  display: flex;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  align-items: center;
+}
+.comment-info{
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
+}
+.commnet-content{
+  font-size: 1.2rem;
+  line-height: 1.5;
+  word-break: keep-all;
+  overflow-wrap: break-word;
 }
 </style>
